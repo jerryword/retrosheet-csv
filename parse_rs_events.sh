8b71693456275eb25fd96df6e0086c7e2220c883
Jerry Word <jerry.word@gmail.com>
2019-09-17 20:45:45 -0700

    add parse_rs_events.sh

---
+++ parse_rs_events.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Parse retrosheet event files
+dirs=(event/asg 
+  event/post 
+  event/regular)
+outdir=../csv
+touch team
+for dir in ${dirs[@]} 
+do
+  for file in $dir/*
+  do
+    if [[ $file =~ EV.$|ED.$ ]]
+    then
+    echo "Processing ${file##*/}"
+      /usr/local/bin/cwgame -q -f 0-83 -x 0-94 -n $file > $outdir/${file##*/}.${dir##*/}.game.csv
+      /usr/local/bin/cwevent -q -f 0-96 -x 0-62 -n $file > $outdir/${file##*/}.${dir##*/}.event.csv
+      echo "GAME_ID,EVENT_ID,COMMENT_TX" > $outdir/${file##*/}.${dir##*/}.comment.csv
+      /usr/local/bin/cwcomment -q $file >> $outdir/${file##*/}.${dir##*/}.comment.csv 
+      /usr/local/bin/cwsub -q -n $file > $outdir/${file##*/}.${dir##*/}.sub.csv
+      /usr/local/bin/cwdaily -q -n $file > $outdir/${file##*/}.${dir##*/}.daily.csv
+    elif [[ $file =~ EB.$ ]]
+    then
+    echo "Processing ${file##*/}"
+    /usr/local/bin/cwdaily -q -n $file > $outdir/${file##*/}.${dir##*/}.daily.csv
+    else
+      continue
+    fi
+  done
+done
+rm team
