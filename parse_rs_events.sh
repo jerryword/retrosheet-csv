a00d50a657e5674c7e1f4104e50de1e2d34221fc
Jerry Word <jerry.word@gmail.com>
2020-09-11 09:50:20 -0700

    fix asg

--- parse_rs_events.sh
+++ parse_rs_events.sh
@@ -16,7 +16,7 @@ do
       /usr/local/bin/cwgame -q -f 0-83 -x 0-94 -n $file > $outdir/${file##*/}.${dir##*/}.game.csv
       /usr/local/bin/cwevent -q -f 0-96 -x 0-62 -n $file > $outdir/${file##*/}.${dir##*/}.event.csv
 #      echo "GAME_ID,EVENT_ID,COMMENT_TX" > $outdir/${file##*/}.${dir##*/}.comment.csv
-      /usr/local/bin/cwcomment -q -f 0-2 $file >> $outdir/${file##*/}.${dir##*/}.comment.csv 
+      /usr/local/bin/cwcomment -q -n -f 0-2 $file > $outdir/${file##*/}.${dir##*/}.comment.csv 
       /usr/local/bin/cwsub -q -n $file > $outdir/${file##*/}.${dir##*/}.sub.csv
       /usr/local/bin/cwdaily -q -n $file > $outdir/${file##*/}.${dir##*/}.daily.csv
     elif [[ $file =~ EB.$ ]]
