49cd64404fbd2c68424b79483b78a564d2185b65
Jerry Word <jerry.word@gmail.com>
2020-09-16 11:04:11 -0700

    fix cwcomment

--- parse_rs_events.sh
+++ parse_rs_events.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Parse retrosheet event files
 dirs=(~/retrosheet/event/asg 
@@ -15,7 +15,6 @@ do
     echo "Processing ${file##*/}"
       /usr/local/bin/cwgame -q -f 0-83 -x 0-94 -n $file > $outdir/${file##*/}.${dir##*/}.game.csv
       /usr/local/bin/cwevent -q -f 0-96 -x 0-62 -n $file > $outdir/${file##*/}.${dir##*/}.event.csv
-#      echo "GAME_ID,EVENT_ID,COMMENT_TX" > $outdir/${file##*/}.${dir##*/}.comment.csv
       /usr/local/bin/cwcomment -q -n -f 0-2 $file > $outdir/${file##*/}.${dir##*/}.comment.csv 
       /usr/local/bin/cwsub -q -n $file > $outdir/${file##*/}.${dir##*/}.sub.csv
       /usr/local/bin/cwdaily -q -n $file > $outdir/${file##*/}.${dir##*/}.daily.csv
@@ -31,18 +30,19 @@ done
 rm team
 
 # Append filename and line number to csv records
-#for file in $outdir/*.csv
-#do
-#  echo "Appending ${file##*/}"
-#  gawk -i inplace '
-#    function basename(file) {
-#      sub(".*/", "", file)
-#      return file
-#    }
-#    {
-#    if (FNR==1) 
-#      { print $0 ",\"" "FILENAME_TX" "\",\"" "LINE_NUMBER_CT" "\"" }
-#      else { print $0 ",\"" basename(FILENAME) "\"," FNR }
-#    }
-#' $file
-#done
+for file in csv/*.csv
+do
+  echo "Appending ${file##*/}"
+  gawk -i inplace '
+    function basename(file) {
+      sub(".*/", "", file)
+      return file
+    }
+    {
+    { gsub(/\n/, ",") }
+    if (FNR==1) 
+      { print $0 ",\"" "FILENAME_TX" "\",\"" "LINE_NUMBER_CT" "\"" }
+      else { print $0 ",\"" basename(FILENAME) "\"," FNR  }
+    }
+' $file
+done
